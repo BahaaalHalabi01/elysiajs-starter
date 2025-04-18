25ad189850cc3d2c81ff804fda8d7613075ad54a
Bahaa <bahaa.alhalabi01@mail.ru>
2025-04-18 12:28:19 +0300

    feat: trpc and swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import { Elysia } from 'elysia'
+import bearer from '@elysiajs/bearer'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+import swagger from '@elysiajs/swagger'
+import { Elysia, t } from 'elysia'
+
+import { compile as c, trpc } from '@elysiajs/trpc'
+import { initTRPC } from '@trpc/server'
+
+const rpc = initTRPC.create()
+const p = rpc.procedure
+
+const router = rpc.router({
+	greet: p.input(c(t.String())).query(({ input }) => input),
+})
+
+const app = new Elysia()
+	.use(swagger({ path: '/docs' }))
+	.use(bearer())
+	.use(trpc(router))
+	.get('/', () => 'Hello Elysia')
+	.listen(3000)
 
 console.log(
 	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
